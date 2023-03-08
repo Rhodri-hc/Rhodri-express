7c3448e04f299a857247214911adc6e343fb03ee
Rhodri-hc <13622450494@163.com>
2023-03-08 23:27:28 +0800

    JWT

--- config/config.default.js
+++ config/config.default.js
@@ -3,4 +3,5 @@
  */
  module.exports = {
     dbURI: "mongodb://localhost:27017/realworld",
+    jwtSecret: "8ba62648-40fa-4afc-820a-928c4868eae6",
   };
--- controller/user.js
+++ controller/user.js
@@ -1,14 +1,33 @@
 const { User } = require("../model");
+const { jwtSecret } = require("../config/config.default");
+const jwt = require('../utils/jwt');
+
 
 // Authentication 用户登录
 exports.login = async (req, res, next) => {
-    try {
-      // 处理请求
-      res.send("post /users/login");
-    } catch (err) {
-      next(err);
-    }
-  };
+  try {
+    // 处理请求
+    // 得到用户信息[mongosse数据对象 转换成 json数据对象]
+    const user = req.user.toJSON();
+    // 生成token
+    const token = await jwt.sign(
+      {
+        userId: user._id,
+      },
+      jwtSecret
+    );
+    // 移除密码属性
+    delete user.password;
+    // 发送成功响应（包含token的用户信息）
+    res.status(200).json({
+      ...user,
+      token,
+    });
+    res.send("post /users/login");
+  } catch (err) {
+    next(err);
+  }
+};
   
   // Registration 用户注册
   exports.register = async (req, res, next) => {
